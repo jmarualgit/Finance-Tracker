706d63718792e1d6926fce247925d0174367eb7d
jmarualgit <jairelmarual@gmail.com>
2023-11-18 13:27:50 -0800

    Added add_row and edit_row
    Used pandas

--- Edit.py
+++ Edit.py
@@ -1,7 +1,20 @@
 import csv
 
+import pandas as pd
 
 def create_csv(fileName, fileLabels):
     with open(fileName, 'w', newline='') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         filewriter.writerow(fileLabels)
+
+def add_row(fileName, data):
+    df = pd.DataFrame(data)
+    
+    df.to_csv(fileName, mode='a', index=False, header=False)
+
+# row number will be +1 what is listed on the left bc of the label on line 1
+def edit_row(fileName, rowNumber, columnName, newValue):
+    df = pd.read_csv(fileName)
+    df.loc[rowNumber, columnName] = newValue
+    
+    df.to_csv(fileName, index=False, header=True)
