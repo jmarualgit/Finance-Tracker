44ce1fdd2948a39ee78d7d38a0f84493a619afdd
jmarualgit <jairelmarual@gmail.com>
2023-11-18 13:28:33 -0800

    Tests
    Includes how to format data for add_row

--- Main.py
+++ Main.py
@@ -5,3 +5,21 @@ import Read as read
 edit.create_csv('test.csv', ['Date Posted', 'Name', 'Amount($)', 'Category', 'Card Used'])
 
 read.read_csv('test.csv')
+
+"""
+data = {
+    'Date Posted' : ['2023-11-17', '2023-11-18'],
+    'Name' : ['test1', 'test2'],
+    'Amount($)' : [10, 12],
+    'Category' : ['Food', 'Bills'],
+    'Card Used' : ['Debit', 'Credit']
+}
+"""
+
+#edit.add_row('data.csv', data)
+#read.read_csv('data.csv')
+
+read.print_csv('data.csv')
+edit.add_row('data.csv', 0, 'Name', 'Changed Name')
+print("edited!\n")
+read.print_csv('data.csv')
