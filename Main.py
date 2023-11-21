7634b574f980598383c495f209da616cb40198f1
jmarualgit <jairelmarual@gmail.com>
2023-11-20 17:47:51 -0800

    Reverted to match current version with merge_csvs function

--- Main.py
+++ Main.py
@@ -1,25 +1,7 @@
-import csv
 import Edit as edit
 import Read as read
+import pandas as pd
 
-edit.create_csv('test.csv', ['Date Posted', 'Name', 'Amount($)', 'Category', 'Card Used'])
+string = 'stmt*.csv'
 
-read.read_csv('test.csv')
-
-"""
-data = {
-    'Date Posted' : ['2023-11-17', '2023-11-18'],
-    'Name' : ['test1', 'test2'],
-    'Amount($)' : [10, 12],
-    'Category' : ['Food', 'Bills'],
-    'Card Used' : ['Debit', 'Credit']
-}
-"""
-
-#edit.add_row('data.csv', data)
-#read.read_csv('data.csv')
-
-read.print_csv('data.csv')
-edit.add_row('data.csv', 0, 'Name', 'Changed Name')
-print("edited!\n")
-read.print_csv('data.csv')
+edit.merge_csv('statements', string)
